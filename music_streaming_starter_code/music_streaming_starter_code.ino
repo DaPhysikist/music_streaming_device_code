de012dabaad4ef437b032dd735e7413c3526bf23
Pranav Mehta <94271480+DaPhysikist@users.noreply.github.com>
2025-02-09 22:44:16 -0800

    Create music_streaming_starter_code.ino

---
+++ music_streaming_starter_code/music_streaming_starter_code.ino
@@ -0,0 +1,23 @@
+#include "Arduino.h"
+#include "WiFi.h"
+#include "Audio.h"
+#define I2S_DOUT     15
+#define I2S_BCLK      16
+#define I2S_LRC        17
+Audio audio;
+String ssid =    "mirkwood";
+String password = "eyeofsauron";
+void setup() {
+  WiFi.disconnect();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid.c_str(), password.c_str());
+  while (WiFi.status() != WL_CONNECTED)
+  delay(1500);
+  audio.setPinout(I2S_BCLK, I2S_LRC, I2S_DOUT);
+  audio.setVolume(100);
+  audio.connecttohost("http://vis.media-ice.musicradio.com/CapitalMP3");
+}
+void loop()
+{
+  audio.loop();
+}
